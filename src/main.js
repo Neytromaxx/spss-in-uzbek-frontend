e3a66432e5b7bbf03582d1e4425aea939e1faab3
Neytromaxx <kadirovsamandar05@gmail.com>
2026-01-11 21:48:35 +0300

    fixing bugs

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import './store/index'
+import store from './store/index'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(store)
+    .mount('#app')
